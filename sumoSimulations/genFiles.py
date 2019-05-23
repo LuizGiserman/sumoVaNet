52233d26e36f56d26e38c4cd22ea2f8e495d83c5
Luiz Giserman <luizgiserman@poli.ufrj.br>
2019-05-23 18:24:15 -0300

    initial commit

---
+++ sumoSimulations/genFiles.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python3
+import os
+import shutil
+import subprocess
+import tempfile
+
+index = ''
+directories = ['comAcostamento/', 'semAcostamento/'] #'comSaidas/', 'comSaidasEntradas/', ]
+endValues = [int(index) for index in range(20, 1001	, 20)]
+
+
+for directory in directories:
+	for value in endValues:
+		# Create a folder named str(value) in each simulation's dir
+		# Create an additional file, considering the str(value) specifications
+		# run sumo with config file, end time and additional file
+
+		#creating value dir
+		if not os.path.exists(directory + str(value)):
+			os.mkdir (directory + str(value))
+
+		#creating additional file
+		f = open ('additional'+str(value)+'.xml', "w+")
+		f.write('<additional>\n\t<edgeData id="myEdges" file="edgesOutput_' + str(value) + '.xml"/>\n\t<laneData id="myLanes" file="lanesOutput_' + str(value) + '.xml"/>\n</additional>')
+
+		#moving additional file
+		shutil.move('additional' + str(value) + '.xml', directory + str(value) + '/' + 'additional' + str(value) + '.xml')
+
+		#running sumo
+		#os.system("ls ./semAcostamento")
+		#print('sumo ' + '-c ./' + directory + 'base.sumocfg ' + '-e' + str(value) + ' --additional-files ./'+ directory + str(value) + '/' 'additional'+str(value)+'.xml')
+
+		#os.system ('sumo ' + '-c ./' + directory + 'base.sumocfg ' + '-e' + str(value) + ' --additional-files ./'+ directory + str(value) + '/' 'additional'+str(value)+'.xml')
+
+
+# sumo -c base.sumocfg -e5000 --additional-file additional5000.xml
+
+
+
+
+
+#<additional>
+#	<edgeData id="myEdges" file="edgesOutput_5000"/>
+#	<laneData id="myLanes" file="lanesOutput_5000"/>
+#</additional>
---
+++ sumoSimulations/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+readonly numDirectories=4
+
+declare -a directories=("comAcostamento" "semAcostamento") #"comSaidas" "comSaidasEntradas" )
+
+
+echo ./genFiles.py
+./genFiles.py
+
+for directory in "${directories[@]}";
+do
+  for value in {20..1001..20}
+  do
+
+    echo sumo -c ./$directory/base.sumocfg -e$value --additional-files ./$directory/$value/additional$value.xml
+    PROC= sumo -c ./$directory/base.sumocfg -e$value --additional-files ./$directory/$value/additional$value.xml
+    echo $PROC
+  done
+
+done
