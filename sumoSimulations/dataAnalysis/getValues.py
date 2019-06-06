4a510da5c0b095267a80aa38e4a3c73ea5832482
Luiz Giserman <luizgiserman@poli.ufrj.br>
2019-06-06 17:24:51 -0300

    Generated Density x time Graphics with pandas and matplotlib

--- sumoSimulations/dataAnalysis/getValues.py
+++ sumoSimulations/dataAnalysis/getValues.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 #comAcostamento
 
@@ -8,26 +11,84 @@ timeValues = [int(index) for index in range(20, 6001, 20)]
 edgePointers = []
 lanePointers = []
 #file's soup List
-edgeSoup = []
+#edgeSoup = []
 laneSoup = []
+#lane tags in a file
+densities =[]
+index = []
+
+dict = {}
 
 for time in timeValues:
     #file names
     auxEdges = "../comAcostamento/{}/edgesOutput_{}.xml".format(time, time)
     auxLanes = "../comAcostamento/{}/lanesOutput_{}.xml".format(time, time)
     #opening all files
-    fpEdges = open(auxEdges, "r")
-    edgePointers.append(fpEdges)
+    #fpEdges = open(auxEdges, "r")
+    #edgePointers.append(fpEdges)
     fpLanes = open(auxLanes, "r")
     lanePointers.append(fpLanes)
 
-for fp in edgePointers:
+#creating all of the lane soups
+for fp in lanePointers:
     soup = BeautifulSoup(fp, "lxml")
-    edgeSoup.append(soup)
+    laneSoup.append(soup)
 
-print(edgeSoup[299].prettify())
+#finding every density in all of the files
+dataFrame = pd.DataFrame()
+s = pd.Series()
+dictLocal = {}
+for count in range(len(laneSoup)):
+    densities = []
+    ids = []
+    for lane in laneSoup[count].find_all('lane'):
+        #density value per id
+        density = lane.get('density')
+        id = lane.get('id')
+        #replace None with zero
+        if (density == None):
+            density = 0
+        #list of densities and ids
+        densities.append(density)
+        ids.append(id)
+        ##### creating the dict ####
+        ## dict needs to be of type {id: list of density values for that id in all files, in order, ...} ##
+        if (count == 0):
+            aux = [density]
+            dictLocal[str(id)] = aux
+        else:
+            auxiliar = dictLocal[str(id)]
+            #add density from current lane to the list
+            auxiliar.append(density)
+            #update the dict with the updated list
+            dictLocal[str(id)] = auxiliar
+
+
+#dataFrame
+dataFrame = pd.DataFrame(dictLocal, index=timeValues)
+
+#print dict
+print("{}\n\n".format(dictLocal))
 
 
 
 
+
+#dataFrame.replace({None : 0}, inplace=True)
+print(dataFrame)
+dataFrame = dataFrame.astype(float)
+ax = dataFrame.plot(title='Densidade')
+ax.set_xlabel("Tempo de simulação (ms)")
+ax.set_ylabel("Densidade (veh/km)")
+plt.show()
+
+
+
+#Traffic volume at the end of the lane / edge (#/h) = 3600 * left / period
+#Traffic volume at the begin of the lane / edge (#/h) = 3600 * entered / period
 #
+# density
+# waitingTime
+# left
+# entered
+# period
